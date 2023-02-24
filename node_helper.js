c41cf4c420206030b8bac0bf9fdcb8b76609d435
zotkinva <v.zotkin@itfbgroup.ru>
2023-02-24 12:37:11 +0300

    no need to translate from Russian. forismatic support it

--- node_helper.js
+++ node_helper.js
@@ -15,15 +15,20 @@ module.exports = NodeHelper.create({
 
     getNewQuote: function () {
         let self = this;
+        let needToTranslate = this.needToTranslate(this.quoteConfig.language);
 
         self.url = this.quoteConfig.url;
-        self.language = this.quoteConfig.language;
+        self.language = needToTranslate ? "en" : this.quoteConfig.language;
 
-        axios.get(self.url)
+        axios.get(self.url, {
+                params: {
+                    lang: self.language
+                }
+            })
             .then(function (response) {
                 self.returned_data = response.data;
 
-                if (self.language !== "en") {
+                if (needToTranslate) {
                     translate(self.returned_data.quoteText, {
                         to: self.language
                     }).then(res => {
@@ -80,6 +85,10 @@ module.exports = NodeHelper.create({
                 res.send({"status": "failed", "error": "No notification given."});
             }
         });
+    },
+
+    needToTranslate: function(language) {
+            return !(language == "en" || language == "ru");
     }
 
 });
